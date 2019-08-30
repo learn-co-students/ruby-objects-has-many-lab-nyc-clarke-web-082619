e7e3a1c6fe741c1c0ebfba23c5e8754879b51f63
jw20191n <clairejw1015@gmail.com>
2019-08-30 14:58:33 -0400

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,21 @@
+class Post
+
+    attr_reader :title
+    attr_accessor :author
+
+    @@all = []
+
+    def initialize(title)
+        @title = title
+        @@all << self
+    end
+
+    def author_name
+        self.author == nil ? nil : self.author.name
+    end
+
+    def self.all
+        @@all
+    end
+
+end
